87626393367b0e51e354e05f7c2c2957eb87e74b
timorss <timorss@gmail.com>
2020-04-20 23:58:44 +0300

    deploy

--- todoProject/settings.py
+++ todoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wqgh5bekrz=z61cebmwc)dy5o8sygi4ib!m_%3)mo3r#3#blno'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://python-django-todo.herokuapp.com/']
 
 
 # Application definition
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
